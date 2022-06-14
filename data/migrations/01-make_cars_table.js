82c5136155a4dae0b794100bcc53d8a10892d618
Jack Nunnink <jack.nunnink@protonmail.com>
2022-06-14 19:21:22 -0400

    db created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', function (table) {
+    table.increments()
+
+    table.string('vin', 17).notNullable().unique()
+    table.string('make', 128).notNullable()
+    table.string('model', 128).notNullable()
+    table.integer('mileage').unsigned().notNullable()
+    table.string('title', 128)
+    table.string('transmission')
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+
+const cars = [
+    {
+        vin: '1HGCM8AJX7U049091',
+        make: 'BMW',
+        model: 'X5',
+        mileage: 10000,
+        title: 'Used 2015 BMW X5 4.8L',
+        transmission: 'Automatic',
+    },
+    {
+        vin: '1HGCM8AJX7U049092',
+        make: 'Ford',
+        model: 'Fiesta',
+        mileage: 20000,
+        title: 'Used 2015 Ford Fiesta 4.8L',
+        transmission: 'Manual',
+    },
+    {
+        vin: '1HGCM8AJX7U049093',
+        make: 'Toyota',
+        model: 'Corolla',
+        mileage: 30000,
+        title: 'Used 2015 Toyota Corolla 4.8L',
+        transmission: 'Automatic',
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
